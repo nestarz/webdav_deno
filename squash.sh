1b4032b1054279f54a08ff2c12759ce7bef9238b
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-04-26 21:14:42 +0200

    chore: remove sh file

--- squash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-original_branch="$1"
-git checkout --orphan new_branch
-git add .
-git commit -m "chore: first commit"
-git branch -M "$original_branch"
-git push -f origin "$original_branch"
